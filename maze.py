c4a752d5d0433bb4b8480d67038e0123dc20d638
ATKincaid <ak247337@muhlenberg.edu>
2015-04-01 20:49:38 -0400

    test 7

--- maze.py
+++ maze.py
@@ -7,7 +7,15 @@ class Maze(object):
     self.image = makePicture('maze.jpg')
     self.w=makeWorld(getWidth(self.image),getHeight(self.image))
     self.w.setPicture(self.image)
-
+    self.t=makeTurtle(self.w)
+    penUp(self.t)
+    moveTo(self.t,30,190)
+  def colorInFront(self.t):
+    """tests to see color in front of turtle"""
+      
+      
+    
+    
 #tests follow here
 doTests=true
 if doTests:
@@ -37,4 +45,18 @@ if doTests:
   except:
     printNow("Test 4 failed, world does not have a picture.")
   #Test 5, testing for existence of a turtle
-  
+  try:
+    if m.t.__class__==Turtle:
+      printNow("Test 5 passed, turtle is in maze.")
+  except:
+     printNow("Test 5 failed, no turtle")
+  #Test 6, test for turtle in right location
+  if getXPos(m.t)==30 and getYPos(m.t)==190:
+    printNow("Test 5 passed, turtle in correct start location.")
+  else:
+    printNow("Test 5 failed, turtle in wrong start location.")
+  #Test 7, check for colorInFront
+  if colorInFront()=colorInFront():
+    printNow("Test 7 passed, colorInFront exists.")
+  else:
+    printNow("Test 7 failed, colorInFront does not exists.")
